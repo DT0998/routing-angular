7c9088b8e99357f64ce1ee4678320e591d6710e7
DT0998 <duongkytrieuvn@gmail.com>
2023-01-04 14:36:48 +0700

    feat: add smth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { AppRoutingModule } from './app-routing.module';
+import { AuthGuard } from './auth-guard.service';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,7 @@ import { AppRoutingModule } from './app-routing.module';
     ServerComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [ServersService],
+  providers: [ServersService, AuthGuard, AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
